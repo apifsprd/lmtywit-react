85bb0afb55e8d751327854ddfd13eb8a6b5307c7
Apif Supriadi <afs.supriadi@gmail.com>
2023-03-20 09:04:00 +0700

    link socmed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -15,22 +15,38 @@ const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            <a
+              href="#"
+              target={"_blank"}
+              className="mr-4 hover:underline md:mr-6 "
+            >
               Discord
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            <a
+              href="https://www.facebook.com/apfsprd/"
+              target={"_blank"}
+              className="mr-4 hover:underline md:mr-6 "
+            >
               Facebook
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a
+              href="https://www.instagram.com/apifsprd/"
+              target={"_blank"}
+              className="mr-4 hover:underline md:mr-6"
+            >
               Instagram
             </a>
           </li>
           <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
+            <a
+              href="https://www.twitter.com/apifsprd/"
+              target={"_blank"}
+              className="mr-4 hover:underline md:mr-6"
+            >
               Twitter
             </a>
           </li>
